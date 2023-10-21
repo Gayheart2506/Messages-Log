2c24785d0f768bde2ac5f9641d19bbd69f6c4bad
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-21 22:07:53 +0000

    added a function and some page content to the new url in the view section

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import datetime
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Hello Django, this is my website")
+
+def hello(request, name):
+    return render(request, "hello/index.html", {
+        "name": name, 
+        "date": datetime.now()
+    })
