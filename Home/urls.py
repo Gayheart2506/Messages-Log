d11937ca35edb730b8a2be90bf992f4dcfe7117a
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-23 01:24:46 +0000

    added some new url paths to the web pages

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="index"),
     path("hello/<str:name>", views.hello, name="hello"),
+    path("about/", views.about_page, name="about"), 
+    path("contact/", views.contact_page, name="contact"),
 ]
